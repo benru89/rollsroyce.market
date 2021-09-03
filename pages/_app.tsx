5eaee45c1d619131ced9a141bcef2de8506293b5
Ruben <rubenfb14@gmail.com>
2021-09-03 22:25:16 +0200

    more renaming of robes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ function Robes({ Component, pageProps }) {
         `}
       </style>
       <Head>
-        <title>robes.market</title>
+        <title>rollsroyce.market</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@400;600;700&display=swap"
@@ -22,19 +22,14 @@ function Robes({ Component, pageProps }) {
         />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:creator" content="@worm_emoji" />
-        <meta property="og:url" content="https://robes.market" />
-        <meta property="og:title" content="robes.market" />
+        <meta property="og:url" content="https://rollsroyce.market" />
+        <meta property="og:title" content="rollsroyce.market" />
         <link rel="icon" type="image/png" href="/favicon.png" />
         <meta
           property="og:description"
           content="See the floor price of Rolls Royce from the Dope Wars project."
         />
-        <meta property="og:image" content="https://robes.market/og.png" />
-        <script
-          data-goatcounter="https://divinerobes.goatcounter.com/count"
-          async
-          src="//gc.zgo.at/count.js"
-        ></script>
+        <meta property="og:image" content="https://rollsroyce-market.vercel.app/og.png" />
       </Head>
     </>
   )
